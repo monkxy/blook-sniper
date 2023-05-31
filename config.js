055427888d41510fabbd503385d279249161f57e
monkey <monkxy@monkxy.org>
2023-05-31 00:32:22 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "user": {
+        "username": "username", // your username
+        "password": "password" // your password
+    },
+    "bazaar": {
+        "item": "blook", // the blook you want to buy
+        "maxprice": "1000", // the maximum price you want to buy the blook for (i recomment keeping this around 1000 if you don't want to lose to many tokens)
+        "minprice": "0" // the minimum price you want to buy the blook for (i recomment keeping this at 0 unless you would like to buy a blook for a specific price)
+    }
+};
